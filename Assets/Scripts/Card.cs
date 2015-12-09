3d65fead097461f7804453eccc268bbe10487518
Tanay Bhargava <tbhargava@blue-shores.com>
2015-12-09 00:28:36 -0800

    Added classes card and deck.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,3 @@
+﻿public class Card {
+	int value;
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Deck {
+
+	List<Card> cards;
+
+	public Deck(List<Card> cards) {
+		this.cards = cards;
+	}
+
+	public void shuffleDeck() {
+
+		Random randomGenerator = new Random (DateTime.Now.Millisecond);
+		List<Card> shuffledCards = new List<Card> ();
+		
+		// randomly remove cards from the one deck and put them in the copied deck
+		while (cards.Count > 0)
+		{
+			int index = randomGenerator.Next(cards.Count);
+			shuffledCards.Add(cards[index]);
+			cards.RemoveAt(index);
+		}
+		cards = shuffledCards;
+	}
+
+	public void discardCards(int numberOfCards) {
+		cards.RemoveRange (0, numberOfCards);
+	}
+
+	public Card getNextCard() {
+		Card card = cards [0];
+		cards.RemoveAt (0);
+		return card;
+	}
+}
